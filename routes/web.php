c5c1b957e3c04a21ea6009e1a7aae42e532d166c
AliUse <121191688+AliUse@users.noreply.github.com>
2026-05-10 10:51:20 +0200

    create Controllers
    CategoryController.
    MenuController.
    ReservationController.
    TableController.

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\ReservationController;
+use App\Http\Controllers\Admin\TableController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('frontend.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -20,6 +24,11 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function(){
 Route::get('/dashboard',[AdminController::class, 'dashboard'])->name('dashboard');
+Route::resource('categories', CategoryController::Class);
+Route::resource('menus', MenuController::Class);
+Route::resource('reservations', ReservationController::Class);
+Route::resource('tables', TableController::Class);
+
 });
 
 require __DIR__.'/auth.php';
