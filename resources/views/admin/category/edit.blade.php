8be61af86ffb58105ed653864b55930a843eec80
AliUse <121191688+AliUse@users.noreply.github.com>
2026-05-17 09:16:37 +0200

    Create edit view for the Category

---
+++ resources/views/admin/category/edit.blade.php
@@ -0,0 +1,16 @@
+<x-admin-layout>
+    <x-slot name="header">
+        {{__('Edit Category')}}
+    </x-slot>
+    
+    <x-alert />
+
+        <form action="{{route('admin.categories.update', $category)}}" method="POST">
+            @csrf
+            @method('PUT')
+
+            @include('admin.category._form') 
+        </form>
+ 
+
+</x-admin-layout>
