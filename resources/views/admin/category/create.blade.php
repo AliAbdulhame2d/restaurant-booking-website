b78d0d4216555cf3b4a97e5a4d3839f1168a7d22
AliUse <121191688+AliUse@users.noreply.github.com>
2026-05-17 09:19:42 +0200

    Create create view for the Category

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,1 +1,14 @@
-<h1>Create Category</h1>
+<x-admin-layout>
+    <x-slot name="header">
+        {{__('Create Category')}}
+    </x-slot>
+    
+    <x-alert />
+
+        <form action="{{route('admin.categories.store')}}" method="POST">
+            @csrf
+            @include('admin.category._form') 
+        </form>
+ 
+
+</x-admin-layout>
