9dda43fba33ce6c82cc4947a87564efb79b032aa
AliUse <121191688+AliUse@users.noreply.github.com>
2026-05-24 12:10:14 +0200

    Update create.blade.php

--- resources/views/admin/menu/create.blade.php
+++ resources/views/admin/menu/create.blade.php
@@ -1,1 +1,14 @@
-<h1>Create Menu</h1>
+<x-admin-layout>
+
+    <x-slot name="header">
+        {{__('Create Menu')}}
+    </x-slot>
+
+
+<form action="{{route('admin.menus.store')}}" method="POST" enctype="multipart/form-data">
+    @csrf
+
+    @include('admin.menu._form')
+</form>
+
+</x-admin-layout>
