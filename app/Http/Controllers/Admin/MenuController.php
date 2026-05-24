143996b0e331c471a5da9be93aba5a983fec6137
AliUse <121191688+AliUse@users.noreply.github.com>
2026-05-24 12:08:50 +0200

    build Menu's Functions
    store();
    edit();
    update();
    destroy();

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Menu;
+use App\Models\Category;
+use App\Http\Requests\StoreMenuRequest;
+use App\Http\Requests\UpdateMenuRequest;
+
 
 class MenuController extends Controller
 {
@@ -13,7 +17,8 @@ class MenuController extends Controller
      */
     public function index()
     {
-        $menus = Menu::with('categories')->get();
+        $menus = Menu::with('category')->get();
+       
         return view('admin.menu.index', compact('menus'));
     }
 
@@ -22,15 +27,24 @@ class MenuController extends Controller
      */
     public function create()
     {
-        return view('admin.menu.create');
+        $categories = Category::all();
+        return view('admin.menu.create', compact('categories'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreMenuRequest $request)
     {
+        $data = $request->validated();
+
+        if($request->hasFile('image')){
+            $data['image'] = $request->file('image')->store('menus', 'public');
+        }
        
+       Menu::create($data);
+
+       return redirect()->back()->with('success', 'Menu Created Successfully');
     }
 
     /**
@@ -44,24 +58,45 @@ class MenuController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Menu $menu)
     {
-        //
+        $categories = Category::all();
+        return view('admin.menu.edit', compact('menu', 'categories'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateMenuRequest $request, Menu $menu)
     {
-        //
+        $data = $request->validated();
+
+        if($request->hasFile('image')){
+
+            if($menu->image){
+                \Storage::disk('public')->delete($menu->image);
+            }
+
+            $data['image'] = $request->file('image')->store('menus', 'public');
+
+        }
+
+        $menu->update($data);
+
+        return redirect()->route('admin.menus.index')->with('success', 'Menu Updated Successfully');
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Menu $menu)
     {
-        //
+        if($menu->image){
+            \Storage::disk('public')->delete($menu->image);
+        }
+
+        $menu->delete();
+        return redirect()->back()->with('success', 'Menu Deleted Successfully');
     }
 }
