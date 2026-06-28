5c5cfb54bc606c3f033437b8bb2a1c27f1411a67
AliUse <121191688+AliUse@users.noreply.github.com>
2026-06-28 19:11:44 +0200

    Update navbar.blade.php

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -2,11 +2,12 @@
     <div class="container mx-auto px-4 py-3 flex justify-between items-center">
         <a href="#" class="text-2xl font-bold text-red-600">Desi Junction</a>
         <div class="hidden md:flex space-x-8">
-            <a href="#home" class="nav-link active">Home</a>
+            <a href="{{route('home')}}" class="nav-link active">Home</a>
             <a href="#menu" class="nav-link">Menu</a>
             <a href="#about" class="nav-link">About</a>
             <a href="#reservation" class="nav-link">Reservations</a>
             <a href="#contact" class="nav-link">Contact</a>
+            <a href="{{route('login')}}" class="nav-link">Login</a>
         </div>
         <button id="mobile-menu-button" class="md:hidden text-gray-700">
             <i class="fas fa-bars text-2xl"></i>
